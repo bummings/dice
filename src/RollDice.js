47bcadb206b57a9640066dc2b6177a56d55e69f5
bummings <eabummings@gmail.com>
2019-07-13 12:35:55 -0400

    all bind everything

--- src/RollDice.js
+++ src/RollDice.js
@@ -8,10 +8,20 @@ class RollDice extends Component {
   constructor(props) {
     super(props);
     this.state = { die1: 'one', die2: 'two' };
+    this.roll = this.roll.bind(this);
   }
 
   roll() {
-    Math.floor(Math.random() * this.props.sides.length);
+    // generate new number
+    const newDie1 = this.props.sides[
+      Math.floor(Math.random() * this.props.sides.length)
+    ];
+    const newDie2 = this.props.sides[
+      Math.floor(Math.random() * this.props.sides.length)
+    ];
+
+    // set new die value
+    this.setState({ die1: newDie1, die2: newDie2 });
   }
 
   // set state with those number
